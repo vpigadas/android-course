e325f4298b9bf17f15bee686fa52b41d2061d7aa
Vasileios Pigadas <vassilis.pigadas@gmail.com>
2020-03-30 21:32:25 +0300

    lecture 2

---
+++ app/src/main/java/com/ath/demo/DemoInterface.java
@@ -0,0 +1,8 @@
+package com.ath.demo;
+
+public interface DemoInterface {
+
+    void onClickAction();
+
+    void onCatException(Exception exception);
+}
--- app/src/main/java/com/ath/demo/MainActivity.java
+++ app/src/main/java/com/ath/demo/MainActivity.java
@@ -1,30 +1,50 @@
 package com.ath.demo;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main2);
+        setContentView(R.layout.activity_login);
 
+        Button btnLogin = findViewById(R.id.login_btn);
+        btnLogin.setText(R.string.brn_login);
 
-        TextView textView = findViewById(R.id.newText);
-        textView.setOnClickListener(new View.OnClickListener() {
+        EditText editUserName = findViewById(R.id.login_username);
+        EditText editPassword = findViewById(R.id.login_password);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String message = "";
 
+                EditText editUserName = findViewById(R.id.login_username);
+                EditText editPassword = findViewById(R.id.login_password);
+
+                message = editUserName.getText().toString();
+
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
             }
         });
-        textView.setOnLongClickListener(new View.OnLongClickListener() {
+
+        btnLogin.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
-                return false;
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+
+
+//                Toast.makeText(MainActivity.this, "Hello Guys!!!!", Toast.LENGTH_SHORT).show();
+                return true;
             }
         });
     }
