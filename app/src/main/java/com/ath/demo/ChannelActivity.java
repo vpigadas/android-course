a0a811461dea73db00797c88213d5a8f6cb619dd
vaspap1790 <vaspap1790@gmail.com>
2020-04-24 17:16:55 +0300

    retrofit added

--- app/src/main/java/com/ath/demo/ChannelActivity.java
+++ app/src/main/java/com/ath/demo/ChannelActivity.java
@@ -5,14 +5,39 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.viewpager.widget.ViewPager;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.ath.demo.communication.ServerResponse;
+import com.ath.demo.communication.retrofit.ApiClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+
 public class ChannelActivity extends AbstractActivity {
 
     MyPageAdapter pageAdapter;
+
+    private boolean isConnected(final ConnectivityManager.OnNetworkActiveListener listener) {
+        try {
+            ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo == null) return false;
+
+            return networkInfo.isConnected() && networkInfo.getType() == ConnectivityManager.TYPE_WIFI;
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        return false;
+    }
 
     @Override
     public int getLayout() {
@@ -30,10 +55,26 @@ public class ChannelActivity extends AbstractActivity {
 //        BlankFragment blankFragment = BlankFragment.newInstance("","");
 //        attachFragment(blankFragment);
 
+        makeARequest();
+
         List<Fragment> fragments = getFragments();
         pageAdapter = new MyPageAdapter(getSupportFragmentManager(), fragments);
         ViewPager pager = (ViewPager) findViewById(R.id.view_pager);
         pager.setAdapter(pageAdapter);
+    }
+
+    private void makeARequest() {
+        ApiClient.getInstance().getTv(new Callback<ServerResponse>() {
+            @Override
+            public void onResponse(Call<ServerResponse> call, retrofit2.Response<ServerResponse> response) {
+                Log.d("COMMUNICATION", response.body().toString());
+                if(response == null){}
+            }
+            @Override
+            public void onFailure(Call<ServerResponse> call, Throwable t) {
+                Toast.makeText(ChannelActivity.this, "Something went wrong...Please try later!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/ath/demo/communication/retrofit/ApiClient.java
@@ -0,0 +1,44 @@
+package com.ath.demo.communication.retrofit;
+
+import com.ath.demo.communication.ServerResponse;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class ApiClient {
+
+    private static ApiClient instance;
+    private static Retrofit retrofit;
+    private static ApiEndpoints endpoints;
+    private static final String BASE_URL = "https://tv-zapping.herokuapp.com/v2/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+
+    public static ApiClient getInstance() {
+        if (instance == null) {
+            instance = new ApiClient();
+
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+            endpoints = ApiClient.getRetrofitInstance().create(ApiEndpoints.class);
+        }
+        return instance;
+    }
+
+    public void getTv(Callback<ServerResponse> listener) {
+        Call<ServerResponse> call = endpoints.getTv();
+        call.enqueue(listener);
+    }
+}
