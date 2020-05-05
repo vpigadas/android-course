080c24c7b9ad71dc9737c362cfa1e2a486cce34f
vaspap1790 <vaspap1790@gmail.com>
2020-05-05 10:24:53 +0300

    ChannelFragment

--- app/src/main/java/com/ath/demo/ChannelFragment.java
+++ app/src/main/java/com/ath/demo/ChannelFragment.java
@@ -4,25 +4,36 @@ import android.os.Bundle;
 
 import android.view.View;
 
+import androidx.recyclerview.widget.RecyclerView;
+
 
 public class ChannelFragment extends AbstractFragment {
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    RecyclerView recyclerView;
+
+    private static final String ARG_PARAM1 = "title";
+    private static final String ARG_PARAM2 = "startTime";
+    private static final String ARG_PARAM3 = "play_icon";
+    private static final String ARG_PARAM4 = "link";
+
 
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private int mParam3;
+    private String mParam4;
 
     public ChannelFragment() {
         // Required empty public constructor
     }
 
-    public static ChannelFragment newInstance(String param1, String param2) {
+    public static ChannelFragment newInstance(String title, String startTime,int play_icon,String link) {
         ChannelFragment fragment = new ChannelFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString(ARG_PARAM1, title);
+        args.putString(ARG_PARAM2, startTime);
+        args.putInt(ARG_PARAM3, play_icon);
+        args.putString(ARG_PARAM4, link);
         fragment.setArguments(args);
         return fragment;
     }
@@ -37,7 +48,11 @@ public class ChannelFragment extends AbstractFragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+            mParam3 = getArguments().getInt(ARG_PARAM3);
+            mParam4 = getArguments().getString(ARG_PARAM4);
         }
+
+
     }
 
     @Override
