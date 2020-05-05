e8bdee02fd800883e7b2bcd9957f768e9d0306f9
vaspap1790 <vaspap1790@gmail.com>
2020-05-05 08:51:36 +0300

    channel icons

--- app/src/main/java/com/ath/demo/MainActivity.java
+++ app/src/main/java/com/ath/demo/MainActivity.java
@@ -15,11 +15,14 @@ public class MainActivity extends AbstractActivity {
 
     RecyclerView recyclerView;
     String channel_names[];
-    String links[] = {"https://webtv.ert.gr/ert1","https://webtv.ert.gr/ert2","https://webtv.ert.gr/ert3","https://www.star.gr/",
-            "https://www.antenna.gr/","https://www.skaitv.gr/","https://www.tvopen.gr/","https://www.alphatv.gr/"};
+    String links[] = {"https://webtv.ert.gr/ert1", "https://webtv.ert.gr/ert2", "https://webtv.ert.gr/ert3", "https://www.star.gr/",
+            "https://www.antenna.gr/", "https://www.skaitv.gr/", "https://www.tvopen.gr/", "https://www.alphatv.gr/"};
     int link_icon = R.drawable.image_preview;
     int channel_icons[] = {R.drawable.ic_channel_ert1, R.drawable.ic_channel_ert2, R.drawable.ic_channel_ert3, R.drawable.ic_channel_star,
             R.drawable.ic_channel_ant1, R.drawable.ic_channel_skai, R.drawable.ic_channel_open, R.drawable.ic_channel_alpha};
+
+//    int channel_icons[] = {R.drawable.ic_channel_vouli, R.drawable.ic_channel_skai, R.drawable.ic_channel_alpha, R.drawable.ic_channel_ert3, R.drawable.ic_channel_ant1,
+//            R.drawable.ic_channel_ert1, R.drawable.ic_channel_ert2, R.drawable.ic_channel_open, R.drawable.ic_channel_star};
 
     @Override
     public int getLayout() {
@@ -32,7 +35,7 @@ public class MainActivity extends AbstractActivity {
         channel_names = getResources().getStringArray(R.array.channel_names);
 
         recyclerView = findViewById(R.id.recyclerView);
-        MyAdapter myAdapter = new MyAdapter(this, channel_icons, channel_names, links ,link_icon);
+        MyAdapter myAdapter = new MyAdapter(this, channel_icons, channel_names, links, link_icon);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
     }
