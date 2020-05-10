0398bea0f8e44c2a69f756f4df550f1148f7ccc4
TetrixGauss <panospavlatos91@gmail.com>
2020-05-10 14:13:42 +0300

    Assignment Part 2 - Change onbackpressed

--- tv-guide/src/main/java/com/applicationgame/tv_guide/ChannelActivity.java
+++ tv-guide/src/main/java/com/applicationgame/tv_guide/ChannelActivity.java
@@ -62,14 +62,15 @@ public class ChannelActivity extends FragmentActivity {
 
     @Override
     public void onBackPressed() {
-        if (mPager.getCurrentItem() == 0) {
-            // If the user is currently looking at the first step, allow the system to handle the
-            // Back button. This calls finish() on this activity and pops the back stack.
-            super.onBackPressed();
-        } else {
-            // Otherwise, select the previous step.
-            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
-        }
+//        if (mPager.getCurrentItem() == 0) {
+//            // If the user is currently looking at the first step, allow the system to handle the
+//            // Back button. This calls finish() on this activity and pops the back stack.
+//            super.onBackPressed();
+//        } else {
+//            // Otherwise, select the previous step.
+//            mPager.setCurrentItem(mPager.getCurrentItem() - 1);
+//        }
+        super.onBackPressed();
     }
 
     private class ScreenSlidePagerAdapter extends FragmentStatePagerAdapter {
--- tv-guide/src/main/java/com/applicationgame/tv_guide/ItemClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.applicationgame.tv_guide;
-
-import android.view.View;
-
-public interface ItemClickListener {
-    void onItemClick(View view, int position);
-}
